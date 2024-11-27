589b678c5316ce97ebc49ddcd1d9c5732e816f5c
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2024-11-27 09:51:58 -0500

    update stable and unstable nixpkgs inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Declarative local dev environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     unstable-nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
